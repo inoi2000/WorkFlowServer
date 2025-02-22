b1183a33c9f38d1c207a94dcf83d4a44f077e9be
inoi2000 <nikolaiklonov@yandex.ru>
2025-02-22 23:48:49 +0500

    initial commit

---
+++ src/main/java/com/petproject/workflow_server/WorkFlowServerApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.workflow_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFlowServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkFlowServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petproject/workflow_server/WorkFlowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.workflow_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFlowServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
