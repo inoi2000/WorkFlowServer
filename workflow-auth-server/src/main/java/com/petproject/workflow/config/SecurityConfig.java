079dd42659a3f81779278c0b8bb8ca578a8be1a5
inoi2000 <nikolaiklonov@yandex.ru>
2025-07-10 15:58:20 +0500

    add auth server

---
+++ workflow-auth-server/src/main/java/com/petproject/workflow/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.petproject.workflow.config;
+
+import com.petproject.workflow.store.User;
+import com.petproject.workflow.store.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Optional;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Order(2)
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            Optional<User> optionalUser = userRepository.findByUsername(username);
+            return optionalUser.orElseThrow(() -> new UsernameNotFoundException("<UNK>"));
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ workflow-auth-server/src/main/java/com/petproject/workflow/store/Role.java
@@ -0,0 +1,8 @@
+package com.petproject.workflow.store;
+
+public enum Role {
+    ROLE_DIRECTOR,
+    ROLE_HR,
+    ROLE_DRIVER,
+    ROLE_ADMIN
+}
