d55df06c6b9021d1982aa5215efd7df2314b1b04
inoi2000 <nikolaiklonov@yandex.ru>
2025-08-02 02:02:56 +0500

    refactoring

--- user-service/src/main/java/com/petproject/workflow/api/SecurityConfig.java
+++ user-service/src/main/java/com/petproject/workflow/api/SecurityConfig.java
@@ -3,21 +3,18 @@ package com.petproject.workflow.api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,13 +27,11 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(HttpMethod.GET, "/api/users/**").hasAuthority("internal")
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").hasAuthority("INTERNAL")
                         .requestMatchers("/api/users", "/api/users/**").hasAuthority("ROLE_ADMIN")
                         .requestMatchers("/", "/**").permitAll())
                 .headers(headers -> headers
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-//                .oauth2ResourceServer(oauth2 -> oauth2
-//                        .jwt(Customizer.withDefaults()))
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter())
@@ -51,30 +46,16 @@ public class SecurityConfig {
 
     private JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
-            // 1. Получаем authorities из claim
-            List<String> userAuthorities = jwt.getClaim("authorities");
-            List<String> scops = jwt.getClaim("scope");
-
-            List<String> authorities = new ArrayList<>();
-            if (userAuthorities != null) {
-                authorities.addAll(userAuthorities);
-            }
-            if (scops != null) {
-                authorities.addAll(scops);
-            }
-
-
-            // 2. Если claim отсутствует или пуст, возвращаем пустой список
-            if (authorities.isEmpty()) {
-                return Collections.emptyList();
-            }
-
-            // 3. Преобразуем в GrantedAuthority
-            return authorities.stream()
-                    .map(SimpleGrantedAuthority::new)
-                    .collect(Collectors.toList());
-        });
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> Stream.of(
+                        jwt.<List<String>>getClaim("authorities"),
+                        jwt.<List<String>>getClaim("scope")
+                )
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .filter(Predicate.not(String::isBlank))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList()));
         return converter;
     }
 }
