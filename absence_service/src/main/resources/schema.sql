476f3b89f30e2aac9994858068e481916bb9f153
inoi2000 <nikolaiklonov@yandex.ru>
2025-09-09 11:24:53 +0500

    add Announcement test and fix bugs

--- absence_service/src/main/resources/schema.sql
+++ absence_service/src/main/resources/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS absences
 (
-    id binary(16) NOT NULL,
-    type varchar(64) NOT NULL,
-    status varchar(64) NOT NULL,
-    start date NOT NULL,
-    end date NOT NULL,
-    place varchar(128),
-    is_approval boolean NOT NULL,
-    employee_id binary(16) NOT NULL,
+    id BINARY(16) NOT NULL,
+    type VARCHAR(64) NOT NULL,
+    status VARCHAR(64) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    place VARCHAR(128),
+    is_approval BOOLEAN NOT NULL,
+    employee_id BINARY(16) NOT NULL,
     PRIMARY KEY (id)
 );
