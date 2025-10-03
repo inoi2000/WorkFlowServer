b04fc09e6286b2c2ef213ad8f5fa0d60a3a1c66e
inoi2000 <124907504+inoi2000@users.noreply.github.com>
2025-10-03 10:53:44 +0500

    Add wenflux to user service (#30)
    * add webflux
    
    * add r2dbc

---
+++ user-service/src/main/java/com/petproject/workflow/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.petproject.workflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Flux;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@EnableWebFluxSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
+        return http
+                .authorizeExchange(request -> request
+                        .pathMatchers("/api/users/auth/**").hasAuthority("INTERNAL")
+                        .pathMatchers("/api/users", "/api/users/**").hasAuthority("ROLE_ADMIN")
+                        .pathMatchers("/", "/**").permitAll())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(reactiveJwtAuthenticationConverter())
+                        ))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter reactiveJwtAuthenticationConverter() {
+        var convertor = new ReactiveJwtAuthenticationConverter();
+        convertor.setJwtGrantedAuthoritiesConverter(jwt -> Flux
+                .just(
+                        Optional.ofNullable(jwt.<List<String>>getClaim("authorities"))
+                                .orElse(Collections.emptyList()),
+                        Optional.ofNullable(jwt.<List<String>>getClaim("scope")).
+                                orElse(Collections.emptyList()))
+                .flatMap(Flux::fromIterable)
+                .filter(Objects::nonNull)
+                .filter(Predicate.not(String::isBlank))
+                .map(SimpleGrantedAuthority::new)
+        );
+        return convertor;
+    }
+}
--- user-service/src/main/java/com/petproject/workflow/store/User.java
+++ user-service/src/main/java/com/petproject/workflow/store/User.java
@@ -1,44 +1,41 @@
 package com.petproject.workflow.store;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
-@Entity
-@Builder
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "users")
+@Table("users")
 public class User {
+
     @Id
-    @Column(name = "id", columnDefinition = "BINARY(16)")
+    @Column("id")
     private UUID id;
 
     @NotNull
-    @Size(min=5, message="Username must be at least 5 characters long")
-    @Column(name = "username", unique = true, nullable = false)
+    @Size(min = 5, message = "Username must be at least 5 characters long")
+    @Column("username")
     private String username;
 
     @NotNull
-    @Size(min=5, message="Password must be at least 5 characters long")
-    @Column(name = "password", nullable = false)
+    @Size(min = 5, message = "Password must be at least 5 characters long")
+    @Column("password")
     private String password;
 
-    @Column(name = "email", unique = true, nullable = false)
+    @NotNull
+    @Column("email")
     private String email;
 
     @NotNull
-    @Enumerated(EnumType.STRING)
-    @Column(name = "role", nullable = false)
+    @Column("role")
     private Role role;
 }
